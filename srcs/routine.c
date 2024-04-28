380311a765c90192c8d2ca9b3dfc020a3f806f0b
Senkyano <rithyyano.hoy@gmail.com>
2024-04-28 23:33:00 +0200

    README + includes

--- srcs/routine.c
+++ srcs/routine.c
@@ -1,34 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosopher.h                                      :+:      :+:    :+:   */
+/*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rihoy <rihoy@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/23 12:45:09 by rihoy             #+#    #+#             */
-/*   Updated: 2024/04/28 22:50:10 by rihoy            ###   ########.fr       */
+/*   Created: 2024/04/28 22:34:16 by rihoy             #+#    #+#             */
+/*   Updated: 2024/04/28 22:43:31 by rihoy            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHER_H
-# define PHILOSOPHER_H
+#include "philosopher.h"
 
-# include <pthread.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <sys/time.h>
-# include "lib_utils.h"
-
-typedef struct s_philo_data
+void	philosopher(void *philo)
 {
-	int				nbr_philo;
-	int				time_to_die;
-	int				time_to_eat;
-	int				time_to_sleep;
-	int				nbr_eat;
-}	t_philo_data;
-
-
-
-#endif
+	t_philo_data	*data;
+}
